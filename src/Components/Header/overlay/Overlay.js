84df5b79369d9d3c762e29f7cf7f2928aa97014c
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-09-14 19:37:17 +0400

    create overlay files

--- src/Components/Header/overlay/Overlay.js
+++ src/Components/Header/overlay/Overlay.js
@@ -1,9 +1,11 @@
 import React from "react";
 import icon from '../../../images/emptyCart.svg';
-import Category from "../Category";
-import iconBox from "../../../images/logo.svg";
-import Currency from "../Currency";
 import CartContext from "../../cart/CartContext";
+import Items from "./Items";
+import ChangeQuantity from "./ChangeQuantity";
+import ImageSlider from "./ImageSlider";
+import Summary from "./Summary";
+import OverlayButtons from "./OverlayButtons";
 
 class Overlay extends React.Component {
     constructor(props) {
@@ -23,32 +25,61 @@ class Overlay extends React.Component {
     render() {
         return (
             <CartContext.Consumer>
-                {({QuantityOfProducts}) => (
-                    <>
-                        <div className={this.state.filterCart ? "basket-container" : "basket-container remove-display"}
+                {({QuantityOfProducts,listOfCartProducts,currencyKey,
+                      ChangeProductInCart,pricesAttributes,cachedData,productsPrices}) => (
+                    <div>
+                        <div className="basket-container"
                              onClick={() => this.OpenCart()}>
                             <img className="basket" src={icon} alt={"basket"}/>
                             <div className="circle">
                                 <span className="circle-value">{QuantityOfProducts()}</span>
                             </div>
                         </div>
-                        <div className={this.state.showCartOverlay ? "overlay" : "remove"}>
-                            <div className="cart-overlay">
-                                <div className="title">
+                        {this.state.showCartOverlay && (
+                            <div className="overlay">
+                                <div className="cart-overlay">
+                                    <div className="title">
                                         <span className="brand">{"My Bag  ,"}</span>
                                         <span className="items">{QuantityOfProducts()} items</span>
-                                    :
-                                    <span>CART</span>
+                                        :
+                                        <span>CART</span>
+                                    </div>
+                                    {listOfCartProducts.map((product,index)=>(
+                                        <div key={index} className="container-overflow-item">
+                                            <Items
+                                                chooseItemID={product.chooseItemID}
+                                                brand={product.brand}
+                                                name={product.name}
+                                                currency={product.prices}
+                                                productAttributes={product.attributes}
+                                                currencyKey={currencyKey}
+                                            />
+                                            <div className="content-right-overflow">
+                                                <ChangeQuantity
+                                                     ChangeProductInCart={ChangeProductInCart}
+                                                    productQuantity={product.length}
+                                                    index={index}
+                                                />
+                                               <ImageSlider
+                                                    images={product.gallery}
+                                                    myKey={index}
+                                                />
+                                            </div>
+                                        </div>
+                                    ))}
+                                    <Summary
+                                        pricesAttributes={pricesAttributes}
+                                        currencyKey={currencyKey}
+                                        prices={cachedData?.map((value, key) => value.length * productsPrices[key])}
+                                        QuantityOfProducts={QuantityOfProducts()}
+                                    />
+                                    <OverlayButtons />
                                 </div>
                             </div>
-
-                        </div>
-
-                    </>
-
+                        )}
+                    </div>
                 )}
             </CartContext.Consumer>
-
         )
     }
 }
