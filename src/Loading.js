1138775b3bb33af50ae0954b2b87c2dddb128885
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-09-18 13:46:12 +0400

    change loading

--- src/Loading.js
+++ src/Loading.js
@@ -4,8 +4,9 @@ import React from "react";
 class Loading extends React.Component {
 
     render() {
-        return(<div>
-            Loading...
+        return(
+            <div>
+            <h1>Loading...</h1>
         </div>)
     }
 }
