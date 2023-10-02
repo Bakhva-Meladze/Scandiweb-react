3a5a270b9bb82527ca861b7472b41d33d1c111a7
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-10-02 23:46:22 +0400

    change App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import CategoryPage from "./Components/CategoryPage/CategoryPage";
 import Header from "./Components/Header/Header";
 import React from "react";
-import {BrowserRouter as Router, Route, Routes, Redirect, BrowserRouter, Switch} from "react-router-dom";
+import {Route, Redirect, BrowserRouter, Switch} from "react-router-dom";
 import ProductPage from "./Components/ProductPage/ProductPage";
 import Cart from "./Components/cart/Cart";
 import "./Style/Style.css";
-import ProductCard from "./Components/CategoryPage/ProductCard";
-import Overlay from "./Components/Header/overlay/Overlay";
 import CartProvider from "./Components/cart/CartProvider";
 import Queres from "./querys/Queres";
-import FetchData from "./FetchData";
-import * as PropTypes from "prop-types";
 import AddProductInCart from "./Components/cart/AddProductInCart";
 import ChangeProductInCart from "./Components/cart/ChangeProductInCart";
-
-
 
 class App extends React.Component {
     render() {
         return (
-                <div className="App">
-                    <Queres>
-                        <AddProductInCart>
-                            <ChangeProductInCart>
-                                <CartProvider>
-                                    <BrowserRouter>
-                                        <Header />
-                                        <Switch>
-                                            <Route path="/category/:item">
-                                                <CategoryPage />
-                                            </Route>
-                                            <Route path="/product/:productId">
-                                                <ProductPage />
-                                            </Route>
-                                            <Route path="/Cart">
-                                                <Cart/>
-                                            </Route>
-                                        </Switch>
-                                    </BrowserRouter>
-                                </CartProvider>
-                            </ChangeProductInCart>
-                        </AddProductInCart>
-                    </Queres>
-                </div>
+            <div className="App">
+                <Queres>
+                    <AddProductInCart>
+                        <ChangeProductInCart>
+                            <CartProvider>
+                                <BrowserRouter>
+                                    <Header/>
+                                    <Switch>
+                                        <Route exact path="/">
+                                            <Redirect to="/category/all"/>
+                                        </Route>
+                                        <Route path="/category/:item">
+                                            <CategoryPage/>
+                                        </Route>
+                                        <Route path="/product/:productId">
+                                            <ProductPage/>
+                                        </Route>
+                                        <Route path="/Cart">
+                                            <Cart/>
+                                        </Route>
+                                    </Switch>
+                                </BrowserRouter>
+                            </CartProvider>
+                        </ChangeProductInCart>
+                    </AddProductInCart>
+                </Queres>
+            </div>
         )
     }
 }
+
 export default App
