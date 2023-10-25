843a9d98162f146a6d3f7bf6dd9909869e23d808
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-10-25 23:25:20 +0400

    delete ProductPage function

--- src/Components/CategoryPage/ProductCard.js
+++ src/Components/CategoryPage/ProductCard.js
@@ -8,38 +8,24 @@ class ProductCard extends React.Component {
         this.state ={
 
         }
-
-        this.ProductPage = this.ProductPage.bind(this);
-
     }
     componentDidMount() {
-        this.props.attributes.map((attributes,key)=>(
+        this.props.productCategory.attributes.map((attributes)=>(
             this.setState({
                 [attributes.id]:0
             })
         ))
     }
-
-    ProductPage() {
-        window.location.pathname = "/product/" + this.props.productCategory.id;
-
-    }
-
     render() {
-/*
-        console.log(this.props.attributes[0]?.id)
-*/
         return (
             <div className="product-card">
-                <span style={{display: this.props.productCategory.inStock === true ? "none" : "block"}}
-                      className={`${this.props.productCategory.inStock === true ? "" : "value-stock"}`}>
+                <span className={`${this.props.productCategory.inStock === true ? "hide" : "value-stock"}`}>
                     {"OUT OF THE STOCK"}
                 </span>
                 <Link to={`/product/${this.props.productCategory.id}`} >
                     <img className={`${this.props.productCategory.inStock === true ? "pictures" : "out-stock"}`}
                          src={this.props.productCategory.gallery[0]}
-                         alt="Product"
-                    />
+                         alt="Product"/>
                 </Link>
                 <div className="container-data">
                     <div className="name"><span>{this.props.productCategory.name}</span>
@@ -51,16 +37,11 @@ class ProductCard extends React.Component {
                     {this.props.productCategory.inStock?
                         <div className="basket-img">
                             <img className="img-value"
-                                 onClick={() => !this.props.productCategory.inStock ? ""
-                                     : this.props.AddProductInCart(
-                                         this.props.productCategory,
-                                         this.state
-                                        /* this.props.productCategory.gallery,
-                                         this.props.productCategory.prices,
-                                         this.props.productCategory.attributes[0]?.items,
-                                         this.props.productCategory.brand,
-                                         this.props.productCategory.name,*/
-                                     )}
+                                 onClick={() => this.props.AddProductInCart
+                                 (
+                                     this.props.productCategory,
+                                     this.state
+                                 )}
                                  src={cartIcon} alt="Cart"/>
                         </div>:""
                     }
