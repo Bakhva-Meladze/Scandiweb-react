39086ca62931fe8f6da20d55b297f699af26ae6c
Bakhva Meladze <88619790+Bakhva-Meladze@users.noreply.github.com>
2023-10-31 19:11:24 +0400

    delete unnecessary things

--- src/Components/cart/UpdateCart.js
+++ src/Components/cart/UpdateCart.js
@@ -1,112 +1,102 @@
 import React, {Component} from "react";
 import CartContext from "./CartContext";
+
 class AddProductInCart extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          cachedData: localStorage.getItem('cartProducts') ? JSON.parse(localStorage.getItem('cartProducts'))
-              : null,
-          currencyKey: localStorage.getItem("currencyKey") ? localStorage.getItem("currencyKey") : 0,
-          productsPrices: [],
-      }
-  }
-  SelectChangeCurrency =(data,array) =>{
+    constructor(props) {
+        super(props);
+        this.state = {
+            cachedData: localStorage.getItem('cartProducts') ? JSON.parse(localStorage.getItem('cartProducts'))
+                : null,
+            currencyKey: localStorage.getItem("currencyKey") ? localStorage.getItem("currencyKey") : 0,
+            productsPrices: [],
+        }
+    }
 
-      this.setState({
-          currencyKey: data,
-          productsPrices: array
-      })
-  }
-  /*componentDidMount() {
-      let arr =[];
-      this.state.cachedData?.map((value, key) => {
-           arr.push(value.prices[this.state.currencyKey]);
+    UpdateCurrency = (data, array) => {
+        this.setState({
+            currencyKey: data,
+            productsPrices: array
+        })
+    }
 
-      });
-      console.log(arr);
-      this.setState({
-          productsPrices: arr
-      });
+    AddProductInCart = (product, choseItemID = false, length = 1) => {
+        const productData = {
+            id: product.id,
+            choseItemID,
+            length: length,
+            gallery: product.gallery,
+            prices: product.prices,
+            items: product.attributes,
+            brand: product.brand,
+            name: product.name,
+        }
 
-  }*/
+        if (JSON.parse(localStorage.getItem("cartProducts"))) {
+            let cachedProducts = JSON.parse(localStorage.getItem("cartProducts"));
+            let newProduct = true;
 
-     AddProductInCart = (product, choseItemID =false,length = 1) => { // (product, chooseItemId = null)
-         const Object = {
-             id: product.id,
-             choseItemID,
-             length: length,
-             gallery: product.gallery,
-             prices: product.prices,
-             items: product.attributes,
-             brand: product.brand,
-             name: product.name,
-         }
-         /*this.setState(prevState => ({
-             productsPrices: [...prevState.productsPrices,product.prices[this.state.currencyKey]],
-         }));*/
-         if (JSON.parse(localStorage.getItem("cartProducts"))) {
-             let add = JSON.parse(localStorage.getItem("cartProducts"));
+            cachedProducts?.forEach((cachedProduct, key) => {
+                if (product.id === cachedProduct.id) {
+                    if (JSON.stringify(choseItemID) === JSON.stringify(cachedProduct.choseItemID)) {
+                        cachedProducts[key].length += 1;
+                        newProduct = false;
 
-             let newItem = true;
+                        this.setState({
+                            cachedData: cachedProducts
+                        });
 
-             add?.forEach((value, key) => {
+                        localStorage.setItem("cartProducts", JSON.stringify(cachedProducts));
+                    }
+                }
+            });
 
-                 if(product.id === add[key].id){
-                     if(JSON.stringify(choseItemID) === JSON.stringify(value.choseItemID)) {
-                         add[key].length += 1;
-                         newItem = false;
-                         this.setState({
-                             cachedData: add
-                         });
-                         localStorage.setItem("cartProducts", JSON.stringify(add));
-                     }
-                 }
-             });
+            if (newProduct) {
+                cachedProducts.push(productData);
+                localStorage.setItem("cartProducts", JSON.stringify(cachedProducts))
+                this.setState({
+                    cachedData: cachedProducts
+                })
+            }
+        } else {
+            localStorage.setItem("cartProducts", JSON.stringify([productData]));
+            this.setState({
+                cachedData: [productData]
+            })
+        }
+    }
 
-             if (newItem) {
-                 add.push(Object);
-                 localStorage.setItem("cartProducts", JSON.stringify(add))
-                 this.setState({
-                     cachedData: add
-                 })
-             }
-         } else {
-             localStorage.setItem("cartProducts", JSON.stringify([Object]));
-             this.setState({
-                 cachedData: [Object]
-             })
-         }
-
-     }
     ChangeProductInCart = (type, key) => {
-        let Object = this.state?.cachedData;
-        Object[key].length += (type === "increase" ? 1 : -1);
+        let cachedCartData = this.state?.cachedData;
+        cachedCartData[key].length += (type === "increase" ? 1 : -1);
 
         if (this.state.cachedData[key].length === 0) {
-            console.log(Object);
-            Object.splice(key, 1);
-            localStorage.setItem("cartProducts", JSON.stringify(Object));
+            cachedCartData.splice(key, 1);
+            localStorage.setItem("cartProducts", JSON.stringify(cachedCartData));
         }
+
         this.setState({
-            cachedData: Object
+            cachedData: cachedCartData
         })
-        localStorage.setItem("cartProducts", JSON.stringify(Object.length < 1 ? null : Object));
+        localStorage.setItem("cartProducts", JSON.stringify(cachedCartData.length < 1 ? null : cachedCartData));
     }
+
     render() {
-        const {queryOfProduct,queryOfCategory,currencyPriceQuery,changeUrl} = this.context;
-        const{cachedData,currencyKey,productsPrices} = this.state;
-        const {AddProductInCart,SelectChangeCurrency,ChangeProductInCart} =this;
+        const {queryOfProduct, queryOfCategory, currencyPriceQuery, changeUrl} = this.context;
+        const {cachedData, currencyKey, productsPrices} = this.state;
+        const {AddProductInCart, UpdateCurrency, ChangeProductInCart} = this;
 
         return (
             <CartContext.Provider value={{
-                queryOfProduct,queryOfCategory,currencyPriceQuery,changeUrl,
-                cachedData,productsPrices,currencyKey,
-                AddProductInCart,SelectChangeCurrency,ChangeProductInCart}}>
+                queryOfProduct, queryOfCategory, currencyPriceQuery, changeUrl,
+                cachedData, productsPrices, currencyKey,
+                AddProductInCart, UpdateCurrency, ChangeProductInCart
+            }}>
                 {this.props.children}
             </CartContext.Provider>
         );
     }
 }
+
 AddProductInCart.contextType = CartContext;
 
 export default AddProductInCart
